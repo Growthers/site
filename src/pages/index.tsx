5c7e6c1fa20556ce8c4856789a02ad8d88a8c763
YukiYuigishi <64316110+YukiYuigishi@users.noreply.github.com>
2021-10-02 23:45:03 +0900

    edit index.tsx

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from "next";
+import Footer from "../components/templates/footer";
+import Header from "../components/templates/header";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
