83307bf710b599b735ffe7361a2260d107df45a9
YukiYuigishi <64316110+YukiYuigishi@users.noreply.github.com>
2021-10-04 09:15:20 +0900

    disable: ESlint error

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,7 @@
 import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 
 class Document extends NextDocument {
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   render() {
     return (
       <Html lang="ja">
