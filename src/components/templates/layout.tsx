9938880668fdcd35c5dbecf03d2d666073a042ee
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2021-10-25 12:08:05 +0900

    fix: format

--- src/components/templates/layout.tsx
+++ src/components/templates/layout.tsx
@@ -4,12 +4,12 @@ import Head from "next/head";
 import Footer from "../organisms/footer";
 import Header from "../organisms/header";
 
-const DefaultDescription = "共同開発鯖のホームぺージ"
+const DefaultDescription = "共同開発鯖のホームぺージ";
 
 interface Information {
   PageTitle: string;
   PageDescription: string;
-};
+}
 
 type Props = {
   PageTitle: string;
@@ -18,61 +18,61 @@ type Props = {
 };
 
 const Metas: FC = () => (
-    <>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <meta name="theme-color" content="#ffffff" />
-    </>
-)
+  <>
+    <meta charSet="utf-8" />
+    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    <meta name="theme-color" content="#ffffff" />
+  </>
+);
 
 const SeoMetas: FC<Information> = ({ PageTitle, PageDescription }) => (
-    <>
-      <meta name="name" content={PageTitle} />
-      {/* <meta name="image" content="" /> */}
-      <meta name="description" content={PageDescription} />
-    </>
-)
+  <>
+    <meta name="name" content={PageTitle} />
+    {/* <meta name="image" content="" /> */}
+    <meta name="description" content={PageDescription} />
+  </>
+);
 
 const OgpMetas: FC<Information> = ({ PageTitle, PageDescription }) => (
-    <>
-      <meta property="og:title" content={PageTitle} />
-      <meta property="og:description" content={PageDescription} />
-      {/* <meta property="og:image" content="" />
+  <>
+    <meta property="og:title" content={PageTitle} />
+    <meta property="og:description" content={PageDescription} />
+    {/* <meta property="og:image" content="" />
       <meta property="og:image:alt" content="共同開発鯖" /> */}
-      <meta property="og:site_name" content="共同開発鯖" />
-    </>
-)
+    <meta property="og:site_name" content="共同開発鯖" />
+  </>
+);
 
 const TwitterMetas: FC<Information> = ({ PageTitle, PageDescription }) => (
-    <>
-      <meta name="twitter:card" content="summary" />
-      <meta name="twitter:title" content={PageTitle} />
-      <meta name="twitter:description" content={PageDescription} />
-      {/* <meta name="twitter:image" content="" /> */}
-    </>
-)
+  <>
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:title" content={PageTitle} />
+    <meta name="twitter:description" content={PageDescription} />
+    {/* <meta name="twitter:image" content="" /> */}
+  </>
+);
 
 const Layout: FC<Props> = ({ PageTitle, children, PageDescription = DefaultDescription }) => (
-    <>
-      <Head>
-        <title>{PageTitle}</title>
-        <Metas />
-        <SeoMetas PageTitle={PageTitle} PageDescription={PageDescription} />
-        <OgpMetas PageTitle={PageTitle} PageDescription={PageDescription} />
-        <TwitterMetas PageTitle={PageTitle} PageDescription={PageDescription} />
-      </Head>
-      <main>
-        <div>
-          <Header />
-          {children}
-        </div>
+  <>
+    <Head>
+      <title>{PageTitle}</title>
+      <Metas />
+      <SeoMetas PageTitle={PageTitle} PageDescription={PageDescription} />
+      <OgpMetas PageTitle={PageTitle} PageDescription={PageDescription} />
+      <TwitterMetas PageTitle={PageTitle} PageDescription={PageDescription} />
+    </Head>
+    <main>
+      <div>
+        <Header />
+        {children}
+      </div>
       <Footer />
     </main>
-    </>
-)
+  </>
+);
 
 Layout.defaultProps = {
   PageDescription: DefaultDescription,
-}
+};
 
 export default Layout;
