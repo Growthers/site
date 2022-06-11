63291769133a4b43877f76af87bd53d2741afa88
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2022-06-11 22:59:29 +0900

    🔥 (Remove): Movile Menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,5 @@
 import type { FC } from "react";
 import Link from "next/link";
-import { stack as Menu } from "react-burger-menu";
-import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 const menu = [
   {
@@ -22,24 +20,6 @@ const menu = [
   },
 ];
 
-const MobileMenu: FC = () => (
-  <Menu
-    right
-    noOverlay
-    customBurgerIcon={<AiOutlineMenu size={36} />}
-    customCrossIcon={<AiOutlineClose color="white" />}
-    className="p-8 top-0 bg-black opacity-80"
-  >
-    {menu.map((value) => (
-      <Link key={value.name} href={value.link}>
-        <a>
-          <p className="p-2 mb-2 text-lg text-white hover:border-b-2 border-orange-400">{value.name}</p>
-        </a>
-      </Link>
-    ))}
-  </Menu>
-);
-
 const Header: FC = () => (
   <div className="flex justify-around md:justify-start items-center h-20 py-4">
     <div className="h-full flex md:ml-8">
@@ -55,9 +35,7 @@ const Header: FC = () => (
         </Link>
       ))}
     </div>
-    <div className="inline-block md:hidden">
-      <MobileMenu />
-    </div>
+    <div className="inline-block md:hidden">{/* Mobile Menu */}</div>
   </div>
 );
 
