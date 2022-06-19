9f862423af0aedf997f3e0a4b027dade9bf53953
Yusuke4869 <67366917+Yusuke4869@users.noreply.github.com>
2022-06-20 06:38:56 +0900

    🎨 (Format): className

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -25,16 +25,16 @@ const Header: FC = () => {
   const [isOpen, setOpen] = useState(false);
 
   return (
-    <div className="flex justify-around md:justify-start items-center h-20 py-4">
-      <div className="h-full flex md:ml-8">
+    <div className="flex h-20 items-center justify-around py-4 md:justify-start">
+      <div className="flex h-full md:ml-8">
         <img src="growthers.png" alt="Growthers" className="rounded-full" />
-        <p className="flex items-center ml-2 text-3xl font-semibold">Growthers</p>
+        <p className="ml-2 flex items-center text-3xl font-semibold">Growthers</p>
       </div>
-      <div className="hidden md:flex items-center ml-20">
+      <div className="ml-20 hidden items-center md:flex">
         {menu.map((value) => (
           <Link key={value.name} href={value.link}>
             <a>
-              <p className="p-2 mx-2 text-lg hover:border-b-2 border-orange-400">{value.name}</p>
+              <p className="mx-2 border-orange-400 p-2 text-lg hover:border-b-2">{value.name}</p>
             </a>
           </Link>
         ))}
@@ -46,17 +46,17 @@ const Header: FC = () => {
         <div
           className={`
           ${isOpen ? "" : "translate-x-full"}
-          fixed top-0 left-[30vw] z-10 h-screen w-[70vw] bg-black transition-all duration-700 ease-in-out text-white overflow-auto
+          fixed top-0 left-[30vw] z-10 h-screen w-[70vw] overflow-auto bg-black text-white transition-all duration-700 ease-in-out
         `}
         >
-          <div className="h-12 w-full flex items-center justify-end">
+          <div className="flex h-12 w-full items-center justify-end">
             <MenuButton toggled={isOpen} toggle={setOpen} color="white" size={24} />
           </div>
           <div className="px-10">
             {menu.map((value) => (
               <Link key={value.name} href={value.link}>
                 <a>
-                  <p className="p-2 mb-2 text-lg text-white hover:border-b-2 border-orange-400">{value.name}</p>
+                  <p className="mb-2 border-orange-400 p-2 text-lg text-white hover:border-b-2">{value.name}</p>
                 </a>
               </Link>
             ))}
